5fe7b90f3c0ec6bacbdfe2ce8cc80d5ad7665f29
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2025-09-26 14:22:31 +0300

    Update fetch_mobile_logs.py

--- scripts/fetch_mobile_logs.py
+++ scripts/fetch_mobile_logs.py
@@ -9,6 +9,7 @@ data = response.json()
 
 df = pd.DataFrame(data["data"])
 
-engine = create_engine("postgresql://peter:1234@4.222.216.225:5432/community_db")
+engine = create_engine("postgresql://peter:pswd4@4.222.216.225:5432/community_db")
 
 df.to_sql("mobile_data", engine, schema="api_log", if_exists="append", index=False)
+
